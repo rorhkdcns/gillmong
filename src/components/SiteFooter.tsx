37a7451e8e94ddeb6b2cbb97e5175ec764a72456
yoopromise <yoopromise@nate.com>
2026-06-22 23:54:45 +0900

    design: 푸터 전체 텍스트 왼쪽 정렬 통일

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -9,11 +9,11 @@ export default function SiteFooter() {
         </p>
 
         {/* 사업자 정보 + 카피라이트 */}
-        <div className="mt-6 flex flex-col gap-1 text-xs text-[#888888] md:text-right">
+        <div className="mt-6 flex flex-col gap-1 text-xs text-[#888888]">
           <span>상호명: 티에이치 컴퍼니 · 대표자: 유태현</span>
           <span>사업자등록번호: 795-44-00873 · 통신판매업신고: 제2026-수원팔달-0211호</span>
           <span>주소: 경기도 수원시 팔달구 정조로900번길 23, 104호</span>
-          <div className="mt-2 flex flex-col gap-1 sm:flex-row sm:items-center sm:justify-between text-gray-400">
+          <div className="mt-2 flex flex-col gap-1 text-gray-400">
             <div className="flex items-center gap-3">
               <a href="/terms"   className="hover:underline">이용약관</a>
               <span>·</span>
